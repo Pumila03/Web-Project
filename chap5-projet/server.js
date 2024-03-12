f2b51cae7b3c0a3949a28abf3b2e82b9d03483b7
Pumila03 <eliott.peaudeau@gmail.com>
2024-03-12 16:19:41 +0100

    modifdossiersplit

--- chap5-projet/server.js
+++ chap5-projet/server.js
@@ -3,72 +3,74 @@ const http = require("http");
 const server = http.createServer();
 const port = 8080;
 let tab = [];
+const images = fs.readdirSync("./public/images/images_small/")
+
 
 
 server.on("request", (request,response) => {
-    if (request.url === "/style.css") {
-        response.end(fs.readFileSync("./public/style.css","utf-8"));
-    } else if (request.url === "/images"){
-        const images = fs.readdirSync("./public/images/images_small/")
+    if (request.url === "/images"){
         let pageHTML = `<!DOCTYPE html>
                         <html>
                             <head>
                                 <title>Les images</title>
                                 <meta charset="UTF-8">
-                                <link rel="stylesheet" href="style.css">
+                                <link rel="stylesheet" href="/public/style.css">
                             </head>
                             <body>
-                                <a href="index.html" class="backhome">Retourner à la page d'accueil</a>
-                                <h1 id="mainwall">Mon mur d'images !</h1>
-                                <div id ="mesimages">`;
+                                <a href="index.html" class="back_to_menu">Retourner à la page d'accueil</a>
+                                <h1 id="wall_images_title">Mon mur d'images !</h1>
+                                <div id ="wall_all_images_container">`;
         for (let i = 0; i < images.length; i++) {
-            pageHTML += `       <a href="page-image/${images[i].slice(0,-10)}">
-                                    <div class ="image-container">
-                                        <img src="./public/images/images_small/${images[i]}" class ="imgc1">
-                                        <div class="hey">Voir plus</div>
+            console.log(images[i])
+            pageHTML += `       <a href="/page-image/${images[i].split("_")[0]}">
+                                    <div class ="wall_image_container">
+                                        <img src="./public/images/images_small/${images[i]}" class ="wall_image">
+                                        <div class="wall_image_text">Voir plus</div>
                                     </div>
                                 </a>`
         }
+        //l'utilisation de plusieurs balises div autours des images sert à créer l'effet de floue et l'affichage du texte
         pageHTML += `       </div>
                         </div>
                     </body>
                 </html>`
         response.end(pageHTML)
     } else if (request.url.startsWith("/page-image/image")){
+        const id = parseInt(request.url.split("/image")[1]);
         let pageHTML = `<!DOCTYPE html>
                             <html>
                                 <head>
                                     <title>Les images</title>
                                     <meta charset="UTF-8">
-                                    <link rel="stylesheet" href="/style.css">
+                                    <link rel="stylesheet" href="/public/style.css">
                                 </head>
                                 <body>
-                                    <a href="/images" class="backhome">
+                                    <a href="/images" class="back_to_menu">
                                         <img src="/public/images/images_normal/camera.png" width=60px>
                                     </a>
                                     <div>
-                                        <img src="/public/images/images_normal/${request.url.slice(12)}.jpg" width=500px class="mainpicture">
-                                        <form action="/image-description${parseInt(request.url.slice(17))}" method="post" class="usercomment">
+                                        <img src="/public/images/images_normal/${request.url.split("image/")[1]}.jpg" width=500px class="mainpicture">
+                                        <form action="/image-description${id}" method="post" class="usercomment">
                                             <input type="text" name="description" placeholder="Votre commentaire...">
                                             <button type="submit">Partager</button>
                                         </form>`
-        if (tab[request.url.slice(17)] != undefined) {                              
-            pageHTML +=                           `<div class="espace_commentaires"><div class="interieur">${tab[parseInt(request.url.slice(17))]}</div></div>`}
+        if (tab[id] != undefined) {                              
+            pageHTML +=                           `<div class="espace_commentaires"><div class="interieur">${tab[id]}</div></div>`}
         pageHTML +=                        `</div>
                                                 <p class="textedescriptif">Le régime paléo nous encourage à manger comme nos ancêtres chasseurs-cueilleurs le faisaient à l’âge de pierre ; il comprend la consommation de poisson, de légumes, de fruits, de viande maigre, d’œufs et de noix, et exclut les produits laitiers, les céréales, les aliments transformés, le café, l’alcool, le sucre et le sel.</p><div class="containerNP">`
-        if (parseInt(request.url.slice(17)) -1 > 0) {
-            pageHTML += `<a href="/page-image/image${String(parseInt(request.url.slice(17)) -1)}" class ="previous">
-                        <p class="p">Image précedente</p>
-                        <img src="/public/images/images_small/image${String(parseInt(request.url.slice(17)) -1)}_small.jpg" alt ="Previous Picture" class="change_image">
-                        </a>`
-        } if (parseInt(request.url.slice(17)) +1 < 54 ) {
-            pageHTML += `<a href="/page-image/image${String(parseInt(request.url.slice(17)) +1)}" class ="next">
-                        <p class="n">Image suivante</p>
-                        <img src="/public/images/images_small/image${String(parseInt(request.url.slice(17)) +1)}_small.jpg" alt ="Next Picture" class="change_image"></a>`
-        }
-        pageHTML += `</div>
-                        </body>
-                            </html>`
+                                                if ( id - 1 > 0) {
+                                                    pageHTML += `<a href="/page-image/image${String((id - 1 + images.length ) % images.length )}" class ="previous">
+                                                        <p class="p">Image précedente</p>
+                                                        <img src="/public/images/images_small/image${String((id - 1  + images.length ) % images.length)}_small.jpg" alt ="Previous Picture" class="change_image">
+                                                    </a>`
+                                                }
+                                                pageHTML += `<a href="/page-image/image${String((id + images.length +1 ) % images.length  )}" class ="next">
+                                                    <p class="n">Image suivante</p>
+                                                    <img src="/public/images/images_small/image${String((id + 1 + images.length) % images.length  )}_small.jpg" alt ="Next Picture" class="change_image">
+                                                </a>
+                                            </div>
+                                        </body>
+                                    </html>`
         response.end(pageHTML)
     } else if (request.method ==="POST" && request.url.startsWith("/image-description")) {
         let donnees
@@ -77,8 +79,9 @@ server.on("request", (request,response) => {
             console.log(donnees)
         })
         request.on("end",()=>{
+            console.log(request.url)
             const paramValeur = donnees.split("&")
-            const number = parseInt(request.url.slice(18))
+            const number = parseInt(request.url.split("description")[1])
             const description = (paramValeur[0].split("=")[1]).replace(/\+/g," ")
             if (tab[number] != undefined) {
                 let temp = tab[number]
@@ -87,9 +90,8 @@ server.on("request", (request,response) => {
             } else {
                 tab[number] = "<p class =\"commentaire\">Commentaire : " + description +"</p>";
             }
-            console.log(tab)
             response.statusCode = 302;
-            response.setHeader('Location', '/page-image/image' + request.url.slice(18));
+            response.setHeader('Location', '/page-image/image' + request.url.split("description")[1]);
             response.end();
 
         })
